f6172d3438bdc55281cce334f8a0f929afe9d879
LumaKEKW <lucasmentel1909@gmail.com>
2023-07-15 21:29:45 +0200

    Input zum Einstempeln     Eingabe prüfen     Zeitstempel abfragen     Zeitstempel ausgeben Programm in While laufen lassen bis Eingabe     Wenn Eingabe         Zeitstempel abfragen Zeitstempel an datetime.datetime übergeben und die Differenz ausrechnen Ausgabe der Ausstempelzeit und der Arbeitszeit

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-from datetime import date
-print('Today´s date:', date)
+from datetime import datetime, timedelta
+
+""" String to datetime object
+Zeitpunkt1 = "15.07.2023 18:37:45"
+time = datetime.strptime(Zeitpunkt1, "%d. %B %Y %H:%M:%S")
+print(time)"""
+
+# Currenttime with format: Hours:Minutes:Seconds Day.Month.Year
+now1 = datetime.now()
+#print(now.strftime("%H:%M:%S %d.%m.%Y"))
+einstempeln = input('Einstempeln?')
+if einstempeln.lower() == 'yes' or einstempeln.lower() == 'ja':
+    timeIn = now1.strftime("%H:%M:%S %d.%m.%Y")
+    print("Eingestempelt: ",timeIn)
+else:
+    exit
+while 1:
+    ausstempeln = input('Beliebige Eingabe zum ausstempeln: ')
+    if ausstempeln.lower() !='':
+        now2 = datetime.now()
+        timeOut = now2.strftime("%H:%M:%S %d.%m.%Y")
+        break
+time1 = datetime.strptime(timeIn, "%H:%M:%S %d.%m.%Y")
+time2 = datetime.strptime(timeOut, "%H:%M:%S %d.%m.%Y")
+timeDif = (time2 - time1)
+print('Ausgestempelt: ',timeOut, '\nSie waren für  ',timeDif , 'eingestempelt!')
