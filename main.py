69620404b9962063d9fc26089e4359b790007726
LumaKEKW <lucasmentel1909@gmail.com>
2023-07-16 09:54:26 +0200

    Fixed While Loop and Feedback Text

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ if einstempeln.lower() == 'yes' or einstempeln.lower() == 'ja':
     timeIn = now1.strftime("%H:%M:%S %d.%m.%Y")
     print("Eingestempelt: ",timeIn)
 else:
-    exit
+    print('Nicht eingestempelt! Gebe Ja oder Yes um sich einzustempeln.')
 # Solange die Schleife läuft, ist man anwesend. Möchte man sich ausstempeln, bricht man die While Schleife mit einer Eingabe ab.    
-while 1:
+while einstempeln.lower() == 'yes' or einstempeln.lower() == 'ja':
     ausstempeln = input('Beliebige Eingabe zum ausstempeln: ')
     if ausstempeln.lower() !='':
         now2 = datetime.now()
         timeOut = now2.strftime("%H:%M:%S %d.%m.%Y")
+        # TimeIn und TimeOut zur einem Datetime Objekt zurückwandeln um anschließend die Differenz zu ermitteln
+        time1 = datetime.strptime(timeIn, "%H:%M:%S %d.%m.%Y")
+        time2 = datetime.strptime(timeOut, "%H:%M:%S %d.%m.%Y")
+        timeDif = (time2 - time1)
+        print('Ausgestempelt: ',timeOut, '\nSie waren für  ',timeDif , 'eingestempelt!')
         break
-# TimeIn und TimeOut zur einem Datetime Objekt zurückwandeln um anschließend die Differenz zu ermitteln
-time1 = datetime.strptime(timeIn, "%H:%M:%S %d.%m.%Y")
-time2 = datetime.strptime(timeOut, "%H:%M:%S %d.%m.%Y")
-timeDif = (time2 - time1)
-print('Ausgestempelt: ',timeOut, '\nSie waren für  ',timeDif , 'eingestempelt!')
