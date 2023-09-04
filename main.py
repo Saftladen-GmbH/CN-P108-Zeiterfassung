6576bfca8dde4243de489f37db336ce4f92b3b76
LumaKEKW <lucasmentel1909@gmail.com>
2023-09-04 12:28:33 +0200

    csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import csv
 
 """ String to datetime object
 Zeitpunkt1 = "15.07.2023 18:37:45"
@@ -18,7 +19,7 @@ while True:
     else:
         print('Nicht eingestempelt! Gebe Ja oder Yes um sich einzustempeln.')
 
-# Solange die Schleife läuft, ist man anwesend. Möchte man sich ausstempeln, bricht man die While Schleife mit einer Eingabe ab.    
+# Solange die Schleife läuft, ist man anwesend. Möchte man sich ausstempeln, bricht man die While Schleife mit einer Eingabe ab
 while True:
     ausstempeln = input('Beliebige Eingabe zum ausstempeln: ')
 
@@ -32,3 +33,10 @@ time1 = datetime.strptime(timeIn, "%H:%M:%S %d.%m.%Y")
 time2 = datetime.strptime(timeOut, "%H:%M:%S %d.%m.%Y")
 timeDif = (time2 - time1)
 print(f'Ausgestempelt: {timeOut} \nSie waren für {timeDif} eingestempelt!')
+
+with open('data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    header = ['Eingestempelt', 'Ausgestempelt', 'Anwesenheit']
+    writer.writerow(header)
+    data = [timeIn, timeOut, timeDif]
+    writer.writerow(data)
