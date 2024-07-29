9f5d506617e8396bb56699a6fb5ead966bfceb2a
Flo <93609174+Xeferis@users.noreply.github.com>
2024-07-29 18:31:07 +0200

    Remove not needed main

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -2,12 +2,8 @@ import pytest
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../")
-from app import main
-
-
-def test_main_add():
-    res = main.adding(1, 2)
-    assert res == 3, "1 + 2 is not equal to 3"
+# Module imports here
+from app import *
 
 
 def test_test():
