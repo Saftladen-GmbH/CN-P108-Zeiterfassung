606d9e3d2620ec30f3799635e2c17fe6f44fca88
Flo <93609174+Xeferis@users.noreply.github.com>
2024-07-29 18:26:25 +0200

    Added small test

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,4 +1,13 @@
 import pytest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../")
+from app import main
+
+
+def test_main_add():
+    res = main.adding(1, 2)
+    assert res == 3, "1 + 2 is not equal to 3"
 
 
 def test_test():
