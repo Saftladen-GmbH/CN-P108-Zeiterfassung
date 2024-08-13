4d30e81e05a3820110013f0762f6292934e8d72e
Flo <93609174+Xeferis@users.noreply.github.com>
2024-08-13 18:32:42 +0200

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/templates/**/*.html", "./app/static/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
