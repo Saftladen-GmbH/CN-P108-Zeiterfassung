66d1bbe346b8e5f6653f078ebf26cd9562a4514a
Lumahigh <lucasmentel1909@gmail.com>
2024-08-20 15:58:16 +0200

    added code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./app/templates/**/*.html", "./app/static/js/**/*.js"],
   theme: {
+    colors:{
+      'fastschwarz': '#C2C2C2',
+      'white': '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
