441a5e167b58c4477e6f6dfbf80a7a787b45025f
Flo <93609174+Xeferis@users.noreply.github.com>
2024-08-26 09:22:54 +0200

    Added blob utils

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -3,11 +3,26 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../")
 # Module imports here
-from utility import random_password, hash_password, verify_password
+from utility import random_password, hash_password, verify_password, image2blob, blob2image
 
 
 def test_test():
     assert 1 == 1, "1 is not equal to 1"
+
+
+def test_image2blob():
+    image_path = "tests/test_image.png"
+    blob = image2blob(image_path)
+    assert isinstance(blob, bytes), "Blob is not of type bytes"
+
+
+def test_blob2image():
+    image_path = "tests/test_image.png"
+    blob = image2blob(image_path)
+    blob2image(blob, "tests/test_image_copy.png")
+    assert os.path.exists("tests/test_image_copy.png"), "Image not saved"
+    os.remove("tests/test_image_copy.png")
+
 
 def test_random_password():
     pw = random_password()
