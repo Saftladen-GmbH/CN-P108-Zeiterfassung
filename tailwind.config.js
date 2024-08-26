4e3ace17b0ce369457ccb7fbf3afbd452f54e2b0
Flo <93609174+Xeferis@users.noreply.github.com>
2024-08-26 09:33:02 +0200

    Added missing colors from branch before

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'fastschwarz': '#2C2C2C'
+        'fastschwarz': '#2C2C2C',
+        'grau-dunkel': '#949494',
+        'grau': '#D9D9D9',
+        'grau-hell': '#F5F5F5',
+        'gelb': '#FCD147',
+        'orange': '#FE833F',
+        'rot': '#FF615D',
+        'dunkelblau': '#035388',
+        'blau': '#0286CA',
+        'türkis': '#30E7f4'
       },
     },
   },
