f104f98cc16bc275945fc72f8666d08f868b1def
Lumahigh <lucasmentel1909@gmail.com>
2024-08-27 19:37:29 +0200

    Code added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'fastschwarz': '#2C2C2C'
+        'fastschwarz': '#2C2C2C',
+        'hellgrau': '#F5F5F5',
+        'grau': '#D9D9D9',
+        'dunkelgrau': '#949494',
+        'gelb': '#FCD147',
+        'orange': '#FE833F',
+        'rot': '#FF615D',
+        'darkblue': '#035388',
+        'blau': '#0286CA',
+        'hellblau': '#30E7F4',
       },
     },
   },
