8ed58e544b5f744d3523aa72f83b8dce2126bb90
Lucas Mentel <lucas@Air-von-Lucas.fritz.box>
2024-09-12 21:10:10 +0200

    Added Code for Anwesenheit - not working atm

--- app/app.py
+++ app/app.py
@@ -80,7 +80,17 @@ def dashboard(userid):
     """
     # user_data = db.session.query(User).filter_by(UID=userid).first()
     user_data = db.get_or_404(User, userid)
-    return render_template("user_dashboard.html", user=user_data)
+    all_logins = user_data.Logins
+    all_logins.sort(key=lambda x: x.Time, reverse=True)
+    all_logins = all_logins[:9]
+    all_logins = [dict(x).update({"tp": "Login"}) for x in all_logins]
+    all_logouts = user_data.Logoffs
+    all_logouts.sort(key=lambda x: x.Time, reverse=True)
+    all_logouts = all_logouts[:9]
+    all_logouts = [dict(x).update({"tp": "Logout"}) for x in all_logouts]
+    total_list = all_logins + all_logouts
+    total_list.sort(key=lambda x: x.Time)
+    return render_template("user_dashboard.html", user=user_data, all_logins=all_logins, all_logouts=all_logouts, total_list=total_list)
 
 @server.route("/user/<userid>/", methods=["POST", "GET"])
 def user(userid: str):
