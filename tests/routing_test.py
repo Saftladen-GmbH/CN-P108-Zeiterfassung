9396f91677596e8de6174cdbfecd9bca8b908c02
Florian <93609174+Xeferis@users.noreply.github.com>
2024-09-23 07:48:56 +0000

    added session test

--- tests/routing_test.py
+++ tests/routing_test.py
@@ -4,6 +4,7 @@ import os
 from pprint import pprint as pp
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../")
 from app import create_app
+from db import init_db, Admin, User, Class, Login, Logoff, generate_uid
 
 def test_app_existence():
     assert create_app
@@ -34,6 +35,16 @@ def test_index(client):
     response = client.get("/")
     assert b"Zeiterfassung by Saftladen GmbH" in response.data
 
+def test_session(client):
+    with client.session_transaction() as session:
+        # set a user id without going through the login route
+        session["userid"] = 'JD0001010004'
+
+    response = client.get("/", follow_redirects=True)
+    assert response.request.path == '/user/JD0001010004'
+    assert b'Einstempeln' in response.data and b'Ausstempeln' in response.data
+
+
 def test_userpage_access_denied(client):
     get_response = client.get("/user/JD0001010004", follow_redirects=True)
     post_response = client.post("user/JD0001010004", data=
@@ -43,9 +54,33 @@ def test_userpage_access_denied(client):
     assert post_response.request.path =='/'
     assert get_response.request.path == '/'
 
+def test_userpage_access(client):
+    with client.session_transaction() as session:
+        # set a user id without going through the login route
+        session["userid"] = 'JD0001010004'
+
+    get_response = client.get("/user/JD0001010004", follow_redirects=True)
+    post_response = client.post("user/JD0001010004", data=
+                                {
+                                    "login": "time_in"
+                                }, follow_redirects=True)
+    assert post_response.status_code == 200
+    assert post_response.request.path == '/user/JD0001010004'
+    assert get_response.request.path == '/user/JD0001010004'
+    assert b'Einstempeln' in get_response.data and b'Ausstempeln' in get_response.data
+
 def test_userdashboard_access_denied(client):
     get_response = client.get("/user/JD0001010004/dashboard", follow_redirects=True)
     assert get_response.request.path == '/'
+
+def test_userdashboard_access(client):
+    with client.session_transaction() as session:
+        # set a user id without going through the login route
+        session["userid"] = 'JD0001010004'
+
+    get_response = client.get("/user/JD0001010004/dashboard", follow_redirects=True)
+    assert get_response.request.path == '/user/JD0001010004/dashboard'
+    assert b'Informationen' in get_response.data
 
 def test_admin_access_denied(client):
     get_response = client.get("/admin/master", follow_redirects=True)
